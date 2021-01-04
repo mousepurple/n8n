1c34b72ddd069fbb2f32e25ebcd90b94b63b9ce2
mousepurple <56299440+mousepurple@users.noreply.github.com>
2021-01-04 15:25:10 +0700

    Update lightsail-compose.sh
    changed the docker compose.yml to point to my own version in this repo.

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -16,7 +16,7 @@ chmod +x /usr/local/bin/docker-compose
 # if you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
-curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/mikegcoleman/todo/master/docker-compose.yml
+curl -o /srv/docker/docker-compose.yml https://github.com/mousepurple/n8n/blob/main/docker-compose.yml
 
 # copy in systemd unit file and register it so our compose file runs 
 # on system restart
